cf15a7b16ca3256e655163852ba41fbeab04b46d
Joris <joris.gallot18@gmail.com>
2025-02-20 11:39:20 +0100

    chore: rename file

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
 use openapiv3::OpenAPI;
 use serde_json;
 
-mod parse_schema;
+mod json_schema_to_typescript;
 
 fn main() {
     let data = include_str!("../openapi_example.json");
     let openapi: OpenAPI = serde_json::from_str(data).expect("Could not deserialize input");
 
     for (name, schema) in openapi.components.unwrap().schemas.iter() {
-        let type_interface = parse_schema::get_interface_from_schema(name, schema);
+        let type_interface = json_schema_to_typescript::get_interface_from_schema(name, schema);
 
         println!("{}", type_interface.to_string());
     }
