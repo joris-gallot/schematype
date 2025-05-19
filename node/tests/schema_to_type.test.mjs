022e74daf727d4aae528dfb331ab8206f06cb3bf
Joris <joris.gallot18@gmail.com>
2025-05-19 22:32:34 +0200

    chore: test preferInterfaceOverType

--- node/tests/schema_to_type.test.mjs
+++ node/tests/schema_to_type.test.mjs
@@ -35,7 +35,7 @@ test('prefer interface over type for simple object', (t) => {
 
   const result = schemaToType(schema, {
     name: 'Person',
-    preferUnknownOverAny: true
+    preferInterfaceOverType: true
   })
 
   t.snapshot(result)
